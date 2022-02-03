9221fae2f10c50c2a040ab6dd33519a1413f5882
pradeep <pradeep@dapi.co>
2022-02-03 12:28:27 +0545

    coin details page connected to firebase

--- src/app/pages/CoinDetails.tsx
+++ src/app/pages/CoinDetails.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-import { collection, getDocs, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc, updateDoc, query, where } from "firebase/firestore";
 import { app, db } from '../../firebase';
 import {
     MixedWidget5,
@@ -66,53 +66,23 @@ const CoinDetails: React.FC<Props> = ({
 }) => {
 
     const [loading, setLoading] = useState(0);
-    const [blogs, setBlogs] = useState([]);
+    const [coinDetail, setDetail] = useState({});
 
     //@ts-ignore
     const { id } = useParams();
-    console.log(id)
 
     useEffect(() => {
-
-
-        const getBlogs = async (category: string) => {
-            setBlogs([])
-            let blogTemp: any = []
-            const querySnapshot = await getDocs(collection(db, "content"));
-            querySnapshot.forEach(async (doc: any) => {
-                if (doc.data().category == category) {
-                    console.log(doc.data().date.toDate().toDateString())
-                    await blogTemp.push({
-                        ...blogTemp,
-                        ...doc.data(),
-                        id: doc.id
-                    })
-                    setBlogs(blogTemp)
-                }
+        const getCoinDetail = async () => {
+            const q = query(collection(db, "coins"), where("symbol", "==", id));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach(async (doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                await setDetail(doc.data())
             });
-            setLoading(1)
         }
-        getBlogs('blog-news')
+        getCoinDetail()
     }, [])
 
-
-    // const renderBlogs = blogs.map((blog) =>
-    //     <div className='col-xl-3'>
-    //         <MixedWidget5
-    //             className='card-xl-stretch mb-5 mb-xl-8'
-    //             image='/media/svg/brand-logos/vimeo.svg'
-    //             //@ts-ignore
-    //             time={String(blog.date.toDate().toDateString())}
-    //             //@ts-ignore
-    //             title={blog.title}
-    //             //@ts-ignore
-    //             description={blog.description}
-    //             //@ts-ignore
-    //             excerpt={blog.excerpt}
-    //             category='blog-news'
-    //         />
-    //     </div>
-    // );
 
 
 
@@ -123,31 +93,30 @@ const CoinDetails: React.FC<Props> = ({
                 <div className='card card-xl-stretch mb-5 mb-xl-8 col-xl-3 m-3'>
                     <div className='card-body d-flex flex-column pb-10 pb-lg-15'>
                         <div className='flex-grow-1'>
-                            <div className='d-flex align-items-center pe-2 mb-5'>
-                                <span className='text fw-bolder fs-6 flex-grow-1 m-5'>
+                            <div className='d-flex align-items-center pe-2 mb-8'>
+                                <span className='text fw-bolder fs-6 flex-grow-1 m-1'>
                                     <div className='d-flex align-items-center'>
                                         <div className='symbol symbol-50px me-5'>
                                             <span className='symbol-label bg-light'>
                                                 <img
                                                     //@ts-ignore
-                                                    src='https://storage.googleapis.com/coinsniper-assets/images/1yoi5dnPcxJlH7PAmmN7PyJ63OFgjV6taryVbcYE.png'
+                                                    src={coinDetail.avatar}
                                                     className='h-75 align-self-end'
                                                     alt=''
                                                 />
                                             </span>
                                         </div>
-                                        <div className='d-flex justify-content-start flex-column'>
-                                            <a href={
-                                                //@ts-ignore
-                                                '/coin-details/aa'
-                                            } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                        <div className='d-flex justify-content-start flex-column mt-4'>
+                                            <a  className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                                 {
                                                     //@ts-ignore
                                                     id.toUpperCase()
                                                 }
                                             </a>
                                             <span className='text-muted fw-bold text-muted d-block fs-7'>
-                                                VeChain
+                                                {
+                                                    //@ts-ignore
+                                                    coinDetail.name}
                                             </span>
 
                                         </div>
@@ -157,58 +126,52 @@ const CoinDetails: React.FC<Props> = ({
                                 </span>
                             </div>
                             <div className='d-flex justify-content-start flex-column'>
-                                <a href={
-                                    //@ts-ignore
-                                    '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                <a  className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                     Market Cap
                                 </a>
                                 <span className='text-muted fw-bold text-muted d-block fs-7'>
-                                    $874,548,214,210.00
+
+                                    {
+                                        //@ts-ignore
+                                        coinDetail.mCap == 0 ? '--' : '$' + Number(coinDetail.mCap).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}
                                 </span>
 
                             </div>
                             <div className='d-flex justify-content-start flex-column mt-3'>
-                                <a href={
-                                    //@ts-ignore
-                                    '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                <a  className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                     Status : <span className="badge badge-square badge-primary fs-6 p-3">
-                                        Promoted
+                                        {
+                                        //@ts-ignore
+                                        coinDetail.status}
                                     </span>
                                 </a>
 
 
                             </div>
                             <div className='d-flex justify-content-start flex-column mt-3'>
-                                <a href={
-                                    //@ts-ignore
-                                    '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                <a  className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                     Network : <span className="text-muted fw-bold text-muted fs-7">
-                                        BSC
+                                        {
+                                        //@ts-ignore
+                                        coinDetail.network ? coinDetail.network : 'NA'}
                                     </span>
                                 </a>
 
 
                             </div>
                             <div className='d-flex justify-content-start flex-column mt-3'>
-                                <a href={
-                                    //@ts-ignore
-                                    '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                <a  className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                     Votes : <span className="badge badge-square badge-success fs-6 p-3">
-                                        65874
+                                        {
+                                        //@ts-ignore
+                                        coinDetail.votes}
                                     </span>
                                 </a>
 
 
                             </div>
                             <div className='d-flex justify-content-start flex-column mt-3'>
-                                <a href={
-                                    //@ts-ignore
-                                    '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
+                                <a className='text-dark fw-bolder text-hover-primary mb-1 fs-6'>
                                     Share
                                     <div className='col-xl-12 mt-5'>
 
@@ -266,10 +229,27 @@ const CoinDetails: React.FC<Props> = ({
                                     </div>
                                 </a>
 
-
                             </div>
-
                         </div>
+                        {
+              //@ts-ignore
+              coinDetail.vote == 1 &&
+              //@ts-ignore
+              <>
+                <div className="btn-group border border-success rounded" role="group" aria-label="Basic example">
+                  <button type="button" disabled className="btn btn-default btn-sm"><b>{
+                    //@ts-ignore
+                    item.votes}</b></button>
+                   <button type='submit' className='btn btn-sm btn-primary pl-2 pr-5' data-kt-menu-dismiss='true' onClick={() => { 
+                     //@ts-ignore
+                     submitVote(coinDetail.symbol, index, coinDetail.id) 
+                     }}>
+                  VOTE !
+                </button>
+                </div>
+               
+              </>
+            }
                     </div>
                 </div>
 
@@ -298,18 +278,20 @@ const CoinDetails: React.FC<Props> = ({
                                 <a href={
                                     //@ts-ignore
                                     '/coin-details/aa'
-                                } className='text-dark fw-bolder text-hover-primary mb-1 '>
+                                } className='text-dark fw-bolder text-hover-primary mb-1 m-2 '>
                                     Contract Address:
                                 </a>
 
-                                0x7B228294D3D5a3f29dFc877e73310CeEC9a84f2c
-                                <KTSVG
-                                //@ts-ignore
+                                {
+                                        //@ts-ignore
+                                        coinDetail.contractAddress ? coinDetail.contractAddress : 'NA'}
+                                {/* <KTSVG
+                                    //@ts-ignore
                                     onClick={() => { navigator.clipboard.writeText('test') }}
 
                                     path='/media/icons/duotune/art/art005.svg'
                                     className='svg-icon-2x svg-icon-warning'
-                                />
+                                /> */}
                             </div>
                             <div className='col-xl-3 m-5'>
 
@@ -317,20 +299,9 @@ const CoinDetails: React.FC<Props> = ({
                             <div className='d-flex align-items-center pe-2 mb-5'>
 
                                 <span className='text fw-bolder fs-6 flex-grow-1 m-5'>
-
-                                    The Highest Returning Rewards Coin that Earns You Up To 15% Per Month. Just For Owning It…
-
-                                    MetaGold Rewards is a new coin that provides a passive income stream to it’s holders…
-
-                                    AN EASIER WAY TO REACH FINANCIAL FREEDOM…
-
-                                    You earn passive income because of the fees on every transaction.
-                                    These are: 14% on buys (11% rewards + 3% Liquidity Pool) and 19% on every sell (16% rewards + 3% Liquidity Pool). This means every time you check your wallet, you’re going to see more rewards in USDT.
-
-                                    Because of the Binance Smart Chain system, you can rely on your USDT coins being delivered to you daily.
-
-                                    MetaGold gives you the opportunity to achieve financial freedom, whatever that means to you: sitting on the beach or doing whatever it is you want! Especially as an early investor!
-                                    {/* {description} */}
+                                {
+                                        //@ts-ignore
+                                        coinDetail.description}
                                 </span>
                             </div>
 
